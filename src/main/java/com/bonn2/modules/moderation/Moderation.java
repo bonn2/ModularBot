d9b6d903153a04574f6ac81da3e6160f326d35f5
Brett Bonifas <bonifas.fetzer@gmail.com>
2022-02-10 19:51:27 -0500

    Add Moderation module v1.0

---
+++ src/main/java/com/bonn2/modules/moderation/Moderation.java
@@ -0,0 +1,48 @@
+package com.bonn2.modules.moderation;
+
+import com.bonn2.Bot;
+import com.bonn2.modules.Module;
+import com.bonn2.modules.roleselect.RoleSelectListener;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+import static com.bonn2.Bot.commands;
+import static com.bonn2.Bot.logger;
+
+public class Moderation extends Module {
+
+    public Moderation() {
+        version = "v1.0";
+        priority = Priority.POST_JDA_LOW;
+        name = "Moderation";
+    }
+
+    @Override
+    public void registerSettings() {
+
+    }
+
+    @Override
+    public void load() {
+        logger.info("Registering listeners...");
+        Bot.jda.addEventListener(new ModerationListener());
+        logger.info("Creating commands...");
+        commands = commands.addCommands(
+                Commands.slash(
+                        "mod",
+                        "Moderation commands"
+                ).addSubcommands(
+                        new SubcommandData(
+                                "purge",
+                                "Purge messages from a channel"
+                        ).addOption(
+                                OptionType.INTEGER,
+                                "amount",
+                                "The number of messages to purge.",
+                                true
+                        )
+                )
+        );
+    }
+}
---
+++ src/main/java/com/bonn2/modules/moderation/ModerationListener.java
@@ -0,0 +1,46 @@
+package com.bonn2.modules.moderation;
+
+import com.bonn2.modules.core.chatlog.ChatLog;
+import com.bonn2.modules.core.permissions.Permissions;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class ModerationListener extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        // Check permissions
+        if (!event.getName().equals("mod")) return;
+        if (!Permissions.hasPermissionReply(event, Permissions.Level.MOD)) return;
+
+        // Do different stuff based on the different command do sub command thingy
+        switch (Objects.requireNonNull(event.getSubcommandName())) {
+            case "purge" -> {
+                int amount = (int) Objects.requireNonNull(event.getOption("amount")).getAsLong();
+                TextChannel channel = event.getTextChannel();
+                event.reply("Purging %s messages in <#%s>".formatted(amount, channel.getId()))
+                        .setEphemeral(true)
+                        .queue();
+                channel.getIterableHistory()
+                        .takeAsync(amount)
+                        .thenAccept(channel::purgeMessages);
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setTitle("Purged messages!");
+                embedBuilder.setColor(Color.RED);
+                embedBuilder.setDescription("<@%s> (%s) purged %s messages in <#%s>".formatted(
+                        event.getUser().getId(),
+                        event.getUser().getAsTag(),
+                        amount,
+                        channel.getId()
+                ));
+                ChatLog.logChannel.sendMessageEmbeds(embedBuilder.build()).queue();
+            }
+        }
+    }
+}
