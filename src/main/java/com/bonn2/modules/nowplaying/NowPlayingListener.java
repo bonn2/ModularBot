38521073ba20f7db006657beeddb691070c7b86f
Brett Bonifas <bonifas.fetzer@gmail.com>
2022-02-28 21:27:51 -0500

    Check users when they join

--- src/main/java/com/bonn2/modules/nowplaying/NowPlayingListener.java
+++ src/main/java/com/bonn2/modules/nowplaying/NowPlayingListener.java
@@ -4,6 +4,7 @@ import com.bonn2.modules.core.settings.Settings;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.user.update.UserUpdateActivitiesEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -33,5 +34,20 @@ public class NowPlayingListener extends ListenerAdapter {
 
         if (member.getRoles().contains(role))
             event.getGuild().removeRoleFromMember(member, role).queue();
+    }
+
+    @Override
+    public void onGuildMemberJoin(@Nonnull GuildMemberJoinEvent event) {
+        Member member = event.getMember();
+        Role role = Settings.get(module, "role").getAsRole();
+        if (role == null) return;
+        for (Activity activity : member.getActivities()) {
+            if (activity.getType().equals(Activity.ActivityType.PLAYING)
+                    && activity.getName().equalsIgnoreCase(Settings.get(module, "game").getAsString())) {
+                if (!member.getRoles().contains(role))
+                    event.getGuild().addRoleToMember(member, role).queue();
+                return;
+            }
+        }
     }
 }
