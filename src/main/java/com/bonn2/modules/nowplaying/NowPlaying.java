99a8a67895c17b5c6acc5aec31944b51269d4667
Brett Bonifas <bonifas.fetzer@gmail.com>
2022-02-28 21:28:05 -0500

    Check users when the bot starts

--- src/main/java/com/bonn2/modules/nowplaying/NowPlaying.java
+++ src/main/java/com/bonn2/modules/nowplaying/NowPlaying.java
@@ -4,13 +4,18 @@ import com.bonn2.Bot;
 import com.bonn2.modules.Module;
 import com.bonn2.modules.core.settings.Settings;
 import com.bonn2.modules.core.settings.types.Setting;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+import static com.bonn2.Bot.logger;
 
 public class NowPlaying extends Module {
 
     public NowPlaying() {
         name = "NowPlaying";
-        version = "v1.0";
+        version = "v1.1";
         priority = Priority.POST_JDA_LOW;
     }
 
@@ -25,10 +30,34 @@ public class NowPlaying extends Module {
     @Override
     public void load() {
         Bot.jda.addEventListener(new NowPlayingListener(this));
+        logger.info("Checking users");
+        checkUsers();
     }
 
     @Override
     public CommandData[] getCommands() {
         return new CommandData[0];
+    }
+
+    public void checkUsers() {
+        Role role = Settings.get(this, "role").getAsRole();
+        Bot.guild.loadMembers().onSuccess((members -> {
+            for (Member member : members) {
+                boolean isPlaying = false;
+                for (Activity activity : member.getActivities()) {
+                    if (activity.getType().equals(Activity.ActivityType.PLAYING)
+                            && activity.getName().equalsIgnoreCase(Settings.get(this, "game").getAsString())) {
+                        if (!member.getRoles().contains(role)) {
+                            Bot.guild.addRoleToMember(member, role).queue();
+                            isPlaying = true;
+                            break;
+                        }
+                    }
+                }
+                if (!isPlaying)
+                    if (member.getRoles().contains(role))
+                        Bot.guild.removeRoleFromMember(member, role).queue();
+            }
+        }));
     }
 }
