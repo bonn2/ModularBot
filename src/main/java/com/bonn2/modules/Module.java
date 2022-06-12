cf6bfe0299b55473e8b5197a6ead5ace85560228
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2022-06-12 19:53:43 -0400

    Fix loading meta.json data

--- src/main/java/com/bonn2/modules/Module.java
+++ src/main/java/com/bonn2/modules/Module.java
@@ -1,5 +1,6 @@
 package com.bonn2.modules;
 
+import com.bonn2.Bot;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -17,15 +18,16 @@ public abstract class Module {
     protected JsonArray depends;
 
     public Module() {
-        try {
-            InputStream metaStream = this.getClass().getResourceAsStream("meta.json");
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        if (classLoader == null) return;
+        try (InputStream metaStream = classLoader.getResourceAsStream("meta.json")) {
             if (metaStream != null) {
                 JsonObject meta = new Gson().fromJson(new String(metaStream.readAllBytes()), JsonObject.class);
-                name = meta.get("name") == null                ? "NoName"        : meta.get("name").getAsString();
-                version = meta.get("version") == null          ? "v0.0"          : meta.get("version").getAsString();
-                description = meta.get("description") == null  ? ""              : meta.get("description").getAsString();
-                author = meta.get("author") == null            ? ""              : meta.get("author").getAsString();
-                depends = meta.get("depends") == null          ? new JsonArray() : meta.get("depends").getAsJsonArray();
+                name = meta.get("name") == null ? "NoName" : meta.get("name").getAsString();
+                version = meta.get("version") == null ? "v0.0" : meta.get("version").getAsString();
+                description = meta.get("description") == null ? "" : meta.get("description").getAsString();
+                author = meta.get("author") == null ? "" : meta.get("author").getAsString();
+                depends = meta.get("depends") == null ? new JsonArray() : meta.get("depends").getAsJsonArray();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
