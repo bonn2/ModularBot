d66dd588e4c2c8f7f0e7b9b8408f8e970269e836
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2022-11-28 13:02:10 -0500

    Optimize inputs

--- src/main/java/net/bonn2/modules/settings/SettingsTabComplete.java
+++ src/main/java/net/bonn2/modules/settings/SettingsTabComplete.java
@@ -4,7 +4,6 @@ import net.bonn2.Bot;
 import net.bonn2.modules.Module;
 import net.bonn2.utils.StringUtil;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
